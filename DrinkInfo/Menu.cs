476819f677e81cc24c3b10cbce55bcf7c7d54ea3
Elijah Reck <kcerhajile@gmail.com>
2024-05-23 17:26:14 -0400

    Added looping and go back/exit options to menus

--- DrinkInfo/Menu.cs
+++ DrinkInfo/Menu.cs
@@ -11,26 +11,49 @@ namespace DrinkInfo
     {
         public static async Task MainMenu()
         {
-            string category = AnsiConsole.Prompt(
+            string? category;
+            do
+            {
+                category = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .Title("Drink Categories")
-                    .PageSize(11)
+                    .PageSize(12)
                     .AddChoices(new[]
                     {
                         "Ordinary Drink", "Cocktail", "Shake"
                         ,"Other/Unknown", "Cocoa", "Shot", "Coffee / Tea"
                         ,"Homemade Liqueur", "Punch / Party Drink", "Beer"
-                        ,"Soft Drink", "Random"
+                        ,"Soft Drink", "Random", "Exit"
                     }));
-            if (category == "Random") await DrinkMenu(Requests.Random);
-            else await DrinkMenu(Requests.Category + category); 
+                if(category != "Exit")
+                    if (category == "Random") await DrinkMenu(Requests.Random);
+                    else await DrinkMenu(Requests.Category + category);
+            } while (category != "Exit");
+            Console.WriteLine("Goodbye!");
             
         }
 
         private static async Task DrinkMenu(string category)
         {
             AllDrinks? allDrinks  = await API.GetAsync(category);
-            if(allDrinks != null && allDrinks.drinks != null) Console.WriteLine(allDrinks.drinks[0].strDrink);
+            if(allDrinks != null && allDrinks.drinks != null && allDrinks.drinks.Count > 0)
+            {
+                List<string> drinkList = allDrinks.drinks.Select(drink => drink.strDrink).Where(str => str != null).ToList();
+                drinkList.Add("Go back");
+                string? chosenDrink;
+                do
+                {
+                    chosenDrink = AnsiConsole.Prompt(
+                       new SelectionPrompt<string>()
+                           .Title("Drink Categories")
+                           .PageSize(10)
+                           .AddChoices(drinkList));
+                } while (chosenDrink != "Go back");
+               
+            } else
+            {
+                Console.WriteLine("No drinks found");
+            }
         }
     }
 }
